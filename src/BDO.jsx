60e251b6965e7edaf2bc93af5eda0bb03451a1d1
classic4u <classic4utv@gmail.com>
2017-06-29 14:39:10 -0700

    Add some more content

--- src/BDO.jsx
+++ src/BDO.jsx
@@ -4,7 +4,11 @@ import {
   CardText,
   CardTitle,
   Divider,
-  Paper
+  Paper,
+  Table,
+  TableBody,
+  TableRow,
+  TableRowColumn
 } from 'material-ui'
 import React from 'react'
 
@@ -15,14 +19,43 @@ const BDO = () => (
     </Card>
     <Card>
       <CardText>
-        <a target="_blank" href="http://bdoplanner.com/save/[[13],[11046,18,[15125,15035]],[11047,17,[15136,15136]],[11049,18,[-1,-1]],[11015,18,[-1,-1]],[[12027,0],[12007,2]],[[11817,2],[11816,2]],[12220,1],[11613,2],[11210,18,[-1,-1]],[14825,18],[11340,18,[-1,-1]],[-1],[-1,[-1]],[-1],[-1],[-1],[-1]]">Current Build</a>
+        <a target="_blank" href="http://bdoplanner.com/save/[[13],[11046,18,[15125,15035]],[11047,17,[15136,15136]],[11016,17,[15111,15111]],[11015,18,[15118,15116]],[[12027,0],[12007,2]],[[11817,2],[11816,2]],[12220,1],[11613,2],[11210,18,[15605,15026]],[14825,18],[11340,18,[15130,15130]],[-1],[-1,[-1]],[-1],[-1],[-1],[-1]]">Current Build</a>
       </CardText>
     </Card>
     <Card>
       <CardTitle
-        title="BDO Resources"
-        subtitle="Extremely Useful."
+        title="BDO Guides Meta-Guide"
+        subtitle="Some resources I've found really useful with explanations as to why."
       />
+    </Card>
+    <Card>
+      <CardHeader
+        title="Enhancing"
+        actAsExpander={true}
+        showExpandableButton={true}
+      />
+      <CardText expandable={true}>
+        <Table>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow>
+              <TableRowColumn>
+                <a target="_blank" href="https://docs.google.com/spreadsheets/d/1WzAeIFslcWhZ-TudUTrvt4S6ejGF8Uo5FwVqNivfHK0/pubhtml#">BDO Enhancement Tables</a>
+              </TableRowColumn>
+              <TableRowColumn>
+                My bible for figuring out what failstacks to build for my enhancement attempts. Note that if you have artisan's memories to use with memory fragments, that may change the math for you.
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
+              <TableRowColumn>
+                <a target="_blank" href="http://www.incendar.com/failstackoptimizer.php">Incendar's FailStack Optimizer</a>
+              </TableRowColumn>
+              <TableRowColumn>
+                A quick table showing the cheapest method to get to a given number of failstacks.
+              </TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardText>
     </Card>
     <Card>
       <CardHeader
@@ -31,7 +64,18 @@ const BDO = () => (
         showExpandableButton={true}
       />
       <CardText expandable={true}>
-        HA HA! WIP
+        <Table>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow>
+              <TableRowColumn>
+                <a href="https://www.reddit.com/r/blackdesertonline/comments/5orkr7/answers_faq_about_grinding/">FAQ About Grinding</a>
+              </TableRowColumn>
+              <TableRowColumn>
+                A list of grind spots & recommended AP/DP. Quite useful for me when I was first figuring out where to grind post-50.
+              </TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
       </CardText>
     </Card>
     <Card>
@@ -41,20 +85,20 @@ const BDO = () => (
         showExpandableButton={true}
       />
       <CardText expandable={true}>
-        HA HA! WIP
+        <Table>
+          <TableBody displayRowCheckbox={false}>
+            <TableRow>
+              <TableRowColumn>
+                <a target="_blank" href="https://www.youtube.com/watch?v=uH2sN62JUSI">Container Trading Guide</a>
+              </TableRowColumn>
+              <TableRowColumn>
+                The most enjoyable active trading method, IMO.
+              </TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
       </CardText>
     </Card>
-    <Card>
-      <CardHeader
-        title="Enhancing"
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardText expandable={true}>
-        HA HA! WIP
-      </CardText>
-    </Card>
-
   </section>
 )
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => (
       <CardText>
         <p><b>How old are you?</b> 28</p>
         <p><b>Where do you live?</b> Los Angeles</p>
-        <p><b>What do you do for a living?</b> I'm a software developer by trade.  I made enough money coding and trading to take a year to pursue my dreams - Right now, I'm part-time streaming and part-time cryptocurrency trading!</p>
+        <p><b>What do you do for a living?</b> I'm a software developer by career. I'm lucky enough to be taking a year off to pursue my dreams! - Investing in the cryptocurrency sphere, and streaming on twitch.tv</p>
         <p><b>What other stuff do you do?</b> I enjoy playing the guitar, going to concerts, drinking whiskey with friends, tabletop gaming, reading, amongst other stuff.</p>
         <p><b>What's with this "well-rounded" bullshit?</b> I have a dream, that one day, gamers will be regarded as more than Cheeto-dusted, Mountain Dew-ridden, scoliosis-having memesters. I'm trying to build a community that enjoys gaming but supports each other in all aspects of life.</p>
         <p><b>Anything else I should know about you?</b> I've dealt with a lot of anxiety my whole life, and gaming has been one of my outlets since I was about 8 years old. Some things never change, but the way you deal with them does.</p>
