f92fd2fcfa38a4c4c187926ed2351f444769e8d3
classic4u <classic4utv@gmail.com>
2017-07-03 10:55:41 -0700

    Change to a hamburger menu

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,19 +1,47 @@
-import { AppBar, Tabs, Tab } from 'material-ui'
+import { AppBar, Drawer, MenuItem } from 'material-ui'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Header = () => (
-  <header>
-    <AppBar
-      title="classic4u"
-      iconElementLeft={<img src="logo.png" className="logo" alt="Logo" />}
-    />
-    <Tabs>
-      <Tab label={"Root"} value={0} containerElement={<Link to="/" />} />
-      <Tab label={"Stream Setup"} value={1} containerElement={<Link to="/setup" />} />
-      <Tab label={"BDO"} value={2} containerElement={<Link to="/bdo" />} />
-    </Tabs>
-  </header>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { open: false }
+    this.handleToggleMenu = this.handleToggleMenu.bind(this)
+  }
+
+  // TODO: USE A LAMBDA FUNCTION!!!!
+  handleToggleMenu() {
+   this.setState({ open: !this.state.open })
+ }
+
+  render() {
+    return (
+      <header>
+        <AppBar
+          title="classic4u"
+          iconElementRight={<img src="logo.png" className="logo" alt="Logo" />}
+          onLeftIconButtonTouchTap={this.handleToggleMenu}
+        />
+        <Drawer open={this.state.open}>
+          <MenuItem
+            containerElement={<Link to="/" />}
+            primaryText="Home"
+            onTouchTap={this.handleToggleMenu}
+          />
+          <MenuItem
+            containerElement={<Link to="/setup" />}
+            primaryText="Stream Setup"
+            onTouchTap={this.handleToggleMenu}
+          />
+          <MenuItem
+            containerElement={<Link to="/bdo" />}
+            primaryText="BDO"
+            onTouchTap={this.handleToggleMenu}
+          />
+        </Drawer>
+      </header>
+    )
+  }
+}
 
 export default Header
